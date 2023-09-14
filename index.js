02cc60278bb37d06faff466c7eddab1740d97f2a
seasons986 <annalin12345@gmail.com>
2023-09-14 11:28:53 -0400

    condensing

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 window.addEventListener("load", () => {
   // code from https://codepen.io/deepakkadarivel/pen/LrGEdL
-  addDragDrop(document.getElementById('moon'));
-  addDragDrop(document.getElementById('sun'));
+  addDrag(document.getElementById('moon'));
+  addDrag(document.getElementById('sun'));
 
   // add blur?
 
-  function addDragDrop(box) {
-    function onMove(e) {
+  function addDrag(box) {
+    function onMove(e, isMobile = false) {
         e.preventDefault();
-        box.style.left = e.pageX - 20 + 'px';
-        box.style.top = e.pageY - 20 + 'px';
+        if (isMobile) {
+          var touchLocation = e.targetTouches[0];
+          box.style.left = touchLocation.pageX - 30 + 'px';
+          box.style.top = touchLocation.pageY - 30 + 'px';
+        } else {
+          box.style.left = e.pageX - 20 + 'px';
+          box.style.top = e.pageY - 20 + 'px';
+        }
     }
     box.addEventListener('mousedown', function() {
       document.body.addEventListener('mousemove', onMove);
@@ -17,11 +23,6 @@ window.addEventListener("load", () => {
     document.body.addEventListener('mouseup', function(e) {
       document.body.removeEventListener('mousemove', onMove);
     });
-    box.addEventListener('touchmove', function(e) {
-      e.preventDefault();
-      var touchLocation = e.targetTouches[0];
-      box.style.left = touchLocation.pageX - 30 + 'px';
-      box.style.top = touchLocation.pageY - 30 + 'px';
-    });
+    box.addEventListener('touchmove', () => onMove(e, true));
   }
 });
