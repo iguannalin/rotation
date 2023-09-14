c0a2935d19fe9c9a9c08200cd8f4e65de516b2d8
seasons986 <annalin12345@gmail.com>
2023-09-14 13:02:59 -0400

    mathin with a passn

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ window.addEventListener("load", () => {
   const w = window.innerWidth;
   const hOffset = (window.innerHeight/15);
   const h = window.innerHeight;
-  const wHalf = (w/2);
+  const wHalf = (w/2) + 20;
   const hHalf = (h/2) - 20;
+  const butt = document.createElement("button")
+  butt.style.top = hHalf + 'px';
+  butt.style.left = wHalf + 'px';
+  document.body.appendChild(butt);
 
   // math is my passion -- https://stackoverflow.com/questions/30531474/d3js-find-closest-point-on-circle
+  //                       https://stackoverflow.com/questions/5708532/how-to-calculate-diametrically-opposite-point-inside-a-circle
   let radius = 125;
   function findXY(x, y) {
     var dx = x - wHalf,
@@ -28,14 +33,14 @@ window.addEventListener("load", () => {
           const found = findXY(touchLocation.pageX, touchLocation.pageY);
           box.style.left = found.x - 30 + 'px'; // touchLocation.pageX
           box.style.top = found.y - 30 + 'px'; // touchLocation.pageY
-          const found2 = findXY(Math.abs(touchLocation.pageX - (radius*2)), Math.abs(touchLocation.pageY - (radius*2)));
+          const found2 = findXY(wHalf - (touchLocation.pageX - wHalf), hHalf - (touchLocation.pageY - hHalf));
           other.style.left = found2.x - 30 + 'px'; // touchLocation.pageX
           other.style.top = found2.y - 30 + 'px'; // touchLocation.pageY
         } else {
           const found = findXY(e.pageX, e.pageY);
           box.style.left = found.x - 20 + 'px';// e.pageX
           box.style.top = found.y - 20 + 'px';// e.pageY
-          const found2 = findXY(Math.abs(e.pageX - (radius*2)), Math.abs(e.pageY - (radius*2)));
+          const found2 = findXY(wHalf - (e.pageX - wHalf), hHalf - (e.pageY - hHalf));
           other.style.left = found2.x - 20 + 'px';// e.pageX
           other.style.top = found2.y - 20 + 'px';// e.pageY
         }
