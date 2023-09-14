a8d22202fa072588be552ae92f74481ec64cfb90
seasons986 <annalin12345@gmail.com>
2023-09-14 11:23:28 -0400

    happy init

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", () => {
+  // code from https://codepen.io/deepakkadarivel/pen/LrGEdL
+  addDragDrop(document.getElementById('moon'));
+  addDragDrop(document.getElementById('sun'));
+
+  function addDragDrop(box) {
+    function onMove(e) {
+        e.preventDefault();
+        box.style.left = e.pageX - 20 + 'px';
+        box.style.top = e.pageY - 20 + 'px';
+    }
+    box.addEventListener('mousedown', function() {
+      box.addEventListener('mousemove', onMove);
+    })
+    document.body.addEventListener('mouseup', function(e) {
+      box.removeEventListener('mousemove', onMove);
+    });
+    box.addEventListener('touchmove', function(e) {
+      e.preventDefault();
+      var touchLocation = e.targetTouches[0];
+      box.style.left = touchLocation.pageX - 30 + 'px';
+      box.style.top = touchLocation.pageY - 30 + 'px';
+    });
+  }
+});
