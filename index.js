4a9b8cd6bfbf4d1fa2e8c8baaa461e2a1684348d
seasons986 <annalin12345@gmail.com>
2023-09-14 23:45:51 -0400

    having a lot of fun

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ window.addEventListener("load", () => {
   const h = window.innerHeight;
   const wHalf = (w/2) + 20;
   const hHalf = (h/2) - 20;
-  const butt = document.createElement("button")
-  butt.style.top = hHalf + 'px';
-  butt.style.left = wHalf + 'px';
-  document.body.appendChild(butt);
 
   // math is my passion -- https://stackoverflow.com/questions/30531474/d3js-find-closest-point-on-circle
   //                       https://stackoverflow.com/questions/5708532/how-to-calculate-diametrically-opposite-point-inside-a-circle
@@ -21,37 +17,49 @@ window.addEventListener("load", () => {
     newY = hHalf + dy * radius / dist;
     return {x:newX, y:newY};
   }
-  // add blur?
 
   // code from https://codepen.io/deepakkadarivel/pen/LrGEdL
+  let count = 0;
+  let dir = 1;
   function addDrag(box) {
     const other = (box == moon) ? sun : moon;
     function onMove(e, isMobile = false) {
-        e.preventDefault();
-        if (isMobile) {
-          var touchLocation = e.targetTouches[0];
-          const found = findXY(touchLocation.pageX, touchLocation.pageY);
-          box.style.left = found.x - 30 + 'px'; // touchLocation.pageX
-          box.style.top = found.y - 30 + 'px'; // touchLocation.pageY
-          const found2 = findXY(wHalf - (touchLocation.pageX - wHalf), hHalf - (touchLocation.pageY - hHalf));
-          other.style.left = found2.x - 30 + 'px'; // touchLocation.pageX
-          other.style.top = found2.y - 30 + 'px'; // touchLocation.pageY
-        } else {
-          const found = findXY(e.pageX, e.pageY);
-          box.style.left = found.x - 20 + 'px';// e.pageX
-          box.style.top = found.y - 20 + 'px';// e.pageY
-          const found2 = findXY(wHalf - (e.pageX - wHalf), hHalf - (e.pageY - hHalf));
-          other.style.left = found2.x - 20 + 'px';// e.pageX
-          other.style.top = found2.y - 20 + 'px';// e.pageY
-        }
+      box.style.filter = `blur(${count/100}px)`;
+      other.style.filter = `blur(${count/100}px)`;
+      if (count > 300) {
+        sun.innerHTML = "☀️";
+        moon.innerHTML = "🌙";
+        dir *= -1;
+      }
+      e.preventDefault();
+      if (isMobile) {
+        var touchLocation = e.targetTouches[0];
+        const found = findXY(touchLocation.pageX, touchLocation.pageY);
+        box.style.left = found.x - 30 + 'px'; // touchLocation.pageX
+        box.style.top = found.y - 30 + 'px'; // touchLocation.pageY
+        const found2 = findXY(wHalf - (touchLocation.pageX - wHalf), hHalf - (touchLocation.pageY - hHalf));
+        other.style.left = found2.x - 30 + 'px'; // touchLocation.pageX
+        other.style.top = found2.y - 30 + 'px'; // touchLocation.pageY
+      } else {
+        const found = findXY(e.pageX, e.pageY);
+        box.style.left = found.x - 20 + 'px';// e.pageX
+        box.style.top = found.y - 20 + 'px';// e.pageY
+        const found2 = findXY(wHalf - (e.pageX - wHalf), hHalf - (e.pageY - hHalf));
+        other.style.left = found2.x - 20 + 'px';// e.pageX
+        other.style.top = found2.y - 20 + 'px';// e.pageY
+      }
+      count+=dir;
     }
     box.addEventListener('mousedown', function() {
-      document.body.addEventListener('mousemove', onMove);
+      document.addEventListener('mousemove', onMove);
     })
-    document.body.addEventListener('mouseup', function(e) {
-      document.body.removeEventListener('mousemove', onMove);
+    document.addEventListener('mouseup', function(e) {
+      document.removeEventListener('mousemove', onMove);
+      count = 0;
     });
     box.addEventListener('touchmove', (e) => onMove(e, true));
+    document.addEventListener('touchend', () => count=0);
+    onMove({pageX:0,pageY:0,preventDefault:()=>{}}); // mock initial position
   }
 
   const moon = document.getElementById('moon');
